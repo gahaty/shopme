d1f93f384fca00aee80e46df6d0c96fc1a0bc3ad
unknown <gahaty@gmail.com>
2024-05-20 21:09:56 +0200

    Code Filter Function for Users

--- ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,9 +15,12 @@ import com.shopme.common.entity.User;
 public interface UserRepository extends CrudRepository<User, Integer>, PagingAndSortingRepository<User, Integer> {
 	
 	@Query("SELECT u FROM User u WHERE u.email = :email")
-	public User getUserByEmail(@Param("email") String email);
+	public User getUserByEmail(@Param("email") String mail);
 	
 	public Long countById(Integer id);
+	
+	@Query("SELECT u FROM User u WHERE CONCAT (u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
 	
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 	@Modifying
