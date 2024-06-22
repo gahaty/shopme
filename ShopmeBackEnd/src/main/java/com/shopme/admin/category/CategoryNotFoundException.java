0b9523d63a580601d6f35213612d81258eb334e3
unknown <gahaty@gmail.com>
2024-06-22 11:31:11 +0200

    Code Update Category Function

---
+++ ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.shopme.admin.category;
+
+public class CategoryNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 4756003926065713996L;
+
+	public CategoryNotFoundException(String message) {
+		super(message);
+	}
+}
